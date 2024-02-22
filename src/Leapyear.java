2cabd7ba279f7a1b8d2b0d4986a3dbc41df7e922
mishramohit109 <mishramohit109@gmail.com>
2024-02-22 18:13:43 +0530

    5th java program

---
+++ src/Leapyear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Leapyear {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the year :");
+        int l=sc.nextInt();
+        System.out.println();
+        if (l % 4 == 0||l%400==0)
+        {
+            System.out.println("The entered year is a Leap year");
+        }
+
+
+    }
+}
